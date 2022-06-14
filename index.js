ebb5852517069101f274c920a2f22bf5464af0de
qasim5611 <qmuhammad144@gmail.com>
2022-06-14 19:43:06 +0500

    bjhbj

--- index.js
+++ index.js
@@ -19,8 +19,15 @@ app.use(cors());
 
 
 var bodyParser = require("body-parser");
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
+app.use(bodyParser.json({limit: "50mb"}));
+app.use(
+  bodyParser.urlencoded({
+    limit: "50mb",
+    extended: true,
+    parameterLimit: 50000,
+  })
+);
+
 
 const _dirname = path.resolve();
 app.use("uploads", express.static(path.join(_dirname, "uploads")));
