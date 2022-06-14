fc51b20ec582c1b499f6a9e60eef46ab720062c8
qasim5611 <qmuhammad144@gmail.com>
2022-06-14 19:23:30 +0500

    port

--- index.js
+++ index.js
@@ -186,7 +186,7 @@ connectDb();
 
 const server = http.createServer(app);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 server.listen(PORT, function () {
   console.log("server is listning on port " + PORT);
 });
